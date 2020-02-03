0c9ecb19c9cf8d2987a5ed6d9abc5f578f8be0e2
PauloMorillo <pauloan@hotmail.com>
2020-02-03 22:29:56 +0000

    fixed with correct add items in dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,23 +6,32 @@ import json
 import requests
 
 if __name__ == "__main__":
+    ans = requests.get("https://jsonplaceholder.typicode.com/users/")
+    # print(ans.json())
+    dicuser = dict()
+    for datauser in ans.json():
+        dicuser[datauser.get("id")] = datauser.get("username")
     ans2 = requests.get("https://jsonplaceholder.typicode.com/todos/")
     licomplete = []
     idl = 1
-    while True:
-        for dicto in ans2.json():
-            print(dicto)
-            id = dicto.get("userId")
-            print(id, "and type is ", type(id))
-            ans = requests.get("https://jsonplaceholder.typicode.com/users/"+str(id))
-            name = ans.json().get("username")
-            dictask = {"task": dicto.get("title"),
-                       "completed": dicto.get("completed"),
-                       "username": name}
-            licomplete.append(dictask)
-            if idl is not id:
-                break
-            idl = id
-        dictojson = {id: licomplete}
+    lis = []
+    dictojson = dict()
+    for dicto in ans2.json():
+        # print(dicto)
+        id = dicto.get("userId")
+        # print(id, "and type is ", type(id))
+        dictask = {"task": dicto.get("title"),
+                   "completed": dicto.get("completed"),
+                   "username": dicuser.get(id)}
+        licomplete.append(dictask)
+        if idl is not id:
+            # print("poer aquí pasé")
+            dictojson[idl] = licomplete
+            lis.append(dictojson)
+            # print(dictojson)
+            licomplete = []
+        idl = id
+    dictojson[idl] = licomplete
+    # print(dictojson)
     with open("todo_all_employees.json", 'w', encoding="UTF-8") as jsf:
         json.dump(dictojson, jsf)

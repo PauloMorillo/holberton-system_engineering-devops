6a245b6bb61bc09890f00287ea1492909ce0fdff
PauloMorillo <pauloan@hotmail.com>
2020-02-28 01:29:24 +0000

    created directory and script

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# this file fix an error caused by too many files opened in nginx
+exec { 'fix--for-nginx':
+command  => 'sudo sed -i "1i worker_rlimit_nofile 30000;" "limits.conf" | sudo service nginx restart',
+provider => shell,
+cwd      => '/etc/security/',
+path     => '/usr/bin:/usr/sbin:/bin',
+}

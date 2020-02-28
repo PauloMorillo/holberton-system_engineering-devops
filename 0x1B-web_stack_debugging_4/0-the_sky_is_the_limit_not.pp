431a9ee2151394f0644b436acdbc2686450378e7
PauloMorillo <pauloan@hotmail.com>
2020-02-28 04:39:31 +0000

    fixed with working processes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # this file fix an error caused by too many files opened in nginx
 exec { 'fix--for-nginx':
-command  => 'sudo sed -i "1i worker_rlimit_nofile 30000;" "limits.conf" | sudo service nginx restart',
+command  => 'sudo sed -i "s/_processes 4;/_processes 7;/" "nginx.conf" | sudo service nginx restart',
 provider => shell,
-cwd      => '/etc/security/',
+cwd      => '/etc/nginx/',
 path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
